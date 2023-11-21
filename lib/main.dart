b111432cf4dc1eea0ad85b7e1e92d89b56ef9cad
Sumit Pal <sumitpal.1996@gmail.com>
2023-11-21 09:05:07 +0530

    release configs

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,15 @@ import 'package:flutter/services.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_forecast/core/utils/ui_colors.dart';
 import 'package:flutter_forecast/feature/views/screens/home/home.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
   runApp(const MyApp());
+  // whenever your initialization is completed, remove the splash screen:
+  FlutterNativeSplash.remove();
 }
 
 class MyApp extends StatelessWidget {
