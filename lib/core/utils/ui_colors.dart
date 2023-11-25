6ed72730e0c59439bc4a27a7ec9241db18eacfae
Sumit Pal <sumitpal.1996@gmail.com>
2023-11-25 13:14:53 +0530

    changes for acceptence

--- lib/core/utils/ui_colors.dart
+++ lib/core/utils/ui_colors.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class UIColors {
-  static const overall = Color(0xff6B99D2);
-  // morning palette
+  static const overall = Color(0xff03ACEE);
+  static const light = Color(0xffED1B24);
   static const sunlight = Color.fromARGB(255, 255, 205, 128);
 }
